95f26d083be21c504cdbd39ab361868f71824f38
sharifdarjenkyle <warwar.tae@gmail.com>
2024-10-23 02:29:37 +0800

    Updates

---
+++ precache-manifest.1e7448f269b812cb4921a4ec51e7e262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4956c3c57703452aa4c83d4000ddddd",
+    "url": "/github.io/online-agrimercado/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/github.io/online-agrimercado/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/github.io/online-agrimercado/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/github.io/online-agrimercado/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/github.io/online-agrimercado/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "b9c99b37cb351c7175f7",
+    "url": "/github.io/online-agrimercado/static/js/runtime~main.cc4f2373.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/github.io/online-agrimercado/static/media/logo.06e73328.svg"
+  }
+]);
